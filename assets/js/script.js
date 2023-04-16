1f99b9801879ee5613a88991f32411272628b862
Sandrine Poissonnet <spoisson@gmail.com>
2023-04-16 05:36:31 -0700

    cors resolved, couple more slides

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const formEl = document.querySelector('form');
-
+const predictionEl = document.getElementById('prediction');
+console.log(predictionEl);
 function handleSubmitForm(event) {
 	event.preventDefault();
 	const loanApp = {
@@ -17,21 +18,41 @@ function handleSubmitForm(event) {
 		debtinc: document.getElementById('debtinc').value.trim(),
 	};
 
-	console.log(loanApp);
-
+	// http://127.0.0.1:5000/predict?loan=1100&mortdue=25860.0&value=39025.0&yoj=10.5&derog=0.0&delinq=0.0&clage=94.366667&ninq=1.0&clno=9.0&debtinc=34.818262&reason=HomeImp&job=Other
 	// https://loan-default-predictor.herokuapp.com/predict?loan=1100&mortdue=25860.0&value=39025.0&yoj=10.5&derog=0.0&delinq=0.0&clage=94.366667&ninq=1.0&clno=9.0&debtinc=34.818262&reason=HomeImp&job=Other
+	// const url = `https://loan-default-predictor.herokuapp.com/predict?loan=${loanApp.loan}&mortdue=${loanApp.mortdue}&value=${loanApp.value}&yoj=${loanApp.yoj}&derog=${loanApp.derog}&delinq=${loanApp.delinq}&clage=${loanApp.clage}&ninq=${loanApp.ninq}&clno=${loanApp.clno}&debtinc=${loanApp.debtinc}&reason=${loanApp.reason}&job=${loanApp.job}`;
 	const url = `https://loan-default-predictor.herokuapp.com/predict?loan=${loanApp.loan}&mortdue=${loanApp.mortdue}&value=${loanApp.value}&yoj=${loanApp.yoj}&derog=${loanApp.derog}&delinq=${loanApp.delinq}&clage=${loanApp.clage}&ninq=${loanApp.ninq}&clno=${loanApp.clno}&debtinc=${loanApp.debtinc}&reason=${loanApp.reason}&job=${loanApp.job}`;
 
 	console.log(url);
 
-	fetch(url, { mode: 'no-cors' })
+	fetch(url)
 		.then(function (res) {
-			console.log(res);
-			const response = res.json();
-			console.log(response);
+			return res.json();
+		})
+		.then(function ({ prediction }) {
+			displayPrediction(JSON.parse(prediction)[0]);
 		})
 		.catch(function (err) {
 			console.log('error...NO!', err);
 		});
 }
+
 formEl.addEventListener('submit', handleSubmitForm);
+
+function displayPrediction(pred) {
+	predictionEl.innerHTML = '';
+	const decision = document.createElement('h3');
+	if (pred === 0) {
+		decision.textContent = 'APPROVED';
+		decision.setAttribute('class', 'bg-green text-serif aligncenter');
+		decision.setAttribute('style', 'font-weight: 700');
+	} else if (pred === 1) {
+		decision.textContent = 'REJECTED';
+		decision.setAttribute('class', 'bg-red text-serif aligncenter');
+		decision.setAttribute('style', 'font-weight: 700');
+		const explanation = document.createElement('p');
+		explanation.textContent = explanation.setAttribute('class', 'text-intro');
+	}
+	console.log('decision', decision);
+	predictionEl.appendChild(decision);
+}
